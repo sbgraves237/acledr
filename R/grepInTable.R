4406af94676535561a0c51cc37161074d9ed66c0
sbgraves237 <spencer.graves@effectivedefense.org>
2024-12-02 10:35:11 -0600

    fix grepintable

--- R/grepInTable.R
+++ R/grepInTable.R
@@ -1,17 +1,7 @@
-#' grep for each element of a character vector patter in referenceTable
-#'
-#' @param pattern A character vector containing regular expressions to search for matches in refereanceTable[, -1] and return the corresponding element of referenceTable[, 1]
-#' @param referenceTable An object to search for pattern in as.data.frame(referenceTable)[-1]
-#' @param ignore.case FALSE to search for pattern; TRUE to search for toupper(pattern)
-#' @return A character vector containing matches or error messages with names = pattern
-#' @examples
-#' AfgNethAnt <- grepInTable(c('Afghanistan', 'Netherlands Antilles'))
-#' AfgNthAnt <- c(Afghanistan="AFG", "Netherlands Antilles" = 
-#'        "Found in row 179 of referenceTable with column 1 = ''") 
-#' all.equal(AfgNethAnt, AfgHtnAnt)
 grepInTable <- function(pattern, referenceTable = 
-                          rworldmap::countrySynonyms[, -1], 
-                        ignore.case=TRUE){
+                        rworldmap::countrySynonyms[, -1], 
+                        ignore.case=TRUE, 
+                        collapse=', ', ...){
   rT <- as.data.frame(referenceTable)
   if(ignore.case){ 
     ux <- toupper(pattern)
@@ -30,14 +20,14 @@ grepInTable <- function(pattern, referenceTable =
     for(i in 1:nRefs){
       ix <- which(ux[iStr] == ctrySyn[[i]])
       if(length(ix)>0) break
-      ix <- grep(ux[iStr], ctrySyn[[i]])
+      ix <- grep(ux[iStr], ctrySyn[[i]], ...)
       if(length(ix)>0) break  
     }
     if(length(ix)<1){
       out[iStr] <- paste('No match found for', 
                          pattern[iStr])
     } else{
-      out[iStr] <- iso3[ix]
+      out[iStr] <- paste(iso3[ix], collapse=collapse)
       if(nchar(out[iStr])<1){ 
         out[iStr] <- paste('Found in row', ix, 
                 "of referenceTable with column 1 = ''")
