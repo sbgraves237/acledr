a41d8b706cb1b24a47cdb31a0ad06e6391f257e5
sbgraves237 <spencer.graves@effectivedefense.org>
2024-12-07 20:44:17 -0600

    fix findSubdir

--- R/findSubdir.R
+++ R/findSubdir.R
@@ -35,6 +35,8 @@ findSubdir <- function(pkg="^acledr$", wd=getwd(),
 ## 2. Look for subdir in pkgParent 
 ##
   whichDir <- function(path, pattern, ...){
+    cat('In whichDir, path =\n', path, 
+        '\npattern = ', pattern,'\n')
     Dir <- dir(path, all.files = TRUE, ...)
     Subdp <- whichGrep(pattern, Dir)
     if(length(Subdp)>0){
